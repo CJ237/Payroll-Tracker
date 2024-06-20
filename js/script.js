d20338e66e2f9e8c2efe6c2d52512071a3d51ace
CJ <cjthompson562@gmail.com>
2024-06-19 23:29:26 -0500

    Finished script.js file and updated style.css

--- js/script.js
+++ js/script.js
@@ -1,20 +1,58 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
+let index = 1;
+const employeesArray = [{}];
 // Collect employee data
+
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
- 
+  input = true;
+  
+  while(input){
+    if(input === false){
+      index++;
+      return employeesArray;
+    }else{
+      let first = window.prompt("Enter Employees First Name: ");
+      let last = window.prompt("Enter Employees Last Name: ");
+      let sal = window.prompt("Enter Employees Salary: ");
+      employeesArray.push({firstName: first, lastName: last, salary: sal});
+      console.log(employeesArray[index]);
+      console.log(employeesArray[index].salary);
+      input = window.confirm("Would you like to enter more employees?");
+      index++;
+    }
+  }
+  return employeesArray;
 }
 
-// Display the average salary
+//Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+  let average = 0;
+  let temp = 0;
+  for(let i = 1;i < index; i++) {
+    if(isNaN(employeesArray[i].salary)){
+      console.log("not a number");
+    }else{
+      temp = parseFloat(employeesArray[i].salary);
+      average += temp;
+    }
+  }
+  average /= index - 1;
+  console.log(`The average is: ${average.toFixed(2)} and the total number of employees are: ${employeesArray.length - 1}.`);
 }
 
-// Select a random employee
+
+// // // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  let i = Math.floor(Math.random() * index);
+  if (i === 0){
+    i++;
+  }
+  console.log(`First name: ${employeesArray[i].firstName}, Last name: ${employeesArray[i].lastName}, and Salary: ${employeesArray[i].salary}.`);
 }
 
 /*
@@ -32,7 +70,7 @@ const displayEmployees = function(employeesArray) {
   employeeTable.innerHTML = '';
 
   // Loop through the employee data and create a row for each employee
-  for (let i = 0; i < employeesArray.length; i++) {
+  for (let i = 1; i < employeesArray.length; i++) {
     const currentEmployee = employeesArray[i];
 
     const newTableRow = document.createElement("tr");
